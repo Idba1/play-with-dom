0e4c6d22d15084105cdf956ee0ea7d220c3d329f
Idba1 <moniraislamself@gmail.com>
2024-02-13 23:32:42 +0600

    querySelector

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-console.log("hello");
+/* console.log("hello");
 
 const title = document.getElementById('title');
 title.innerText = 'My JS power';
 title.style.color = "blue";
-console.log(title.innerText);
+console.log(title.innerText); */
+
+
+const title2 = document.querySelector('#title');
+title2.textContent = 'I am done with query selector';
+console.log(title2)
