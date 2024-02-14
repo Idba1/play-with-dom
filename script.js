c20d9979df23d753a58f16fd9ec7ba69a1a142a7
Idba1 <moniraislamself@gmail.com>
2024-02-14 08:18:52 +0600

    get elementByClassName

--- script.js
+++ script.js
@@ -6,6 +6,15 @@ title.style.color = "blue";
 console.log(title.innerText); */
 
 
-const title2 = document.querySelector('#title');
+/* const title2 = document.querySelector('#title');
 title2.textContent = 'I am done with query selector';
-console.log(title2)
+console.log(title2) */
+
+const items = document.getElementsByClassName('li-items');
+// console.log(items.length);
+for(let item = 0; item<items.length; item++){
+    const element = items[item];
+    element.style.color = 'green';
+    element.innerText = "amar khushi"
+    console.log(element);
+}
