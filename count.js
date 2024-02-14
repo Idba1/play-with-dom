7d89edcc99467fd1be858e437362a49ffb7e85c2
Idba1 <moniraislamself@gmail.com>
2024-02-14 18:16:30 +0600

    count.js

---
+++ count.js
@@ -0,0 +1,18 @@
+let sum = 0;
+
+function clickMe(){
+    // console.log('hello');
+    const firstElement = document.getElementById('count');
+    // console.log(firstElement);
+    sum += 1;
+    firstElement.innerText = sum;
+}
+
+
+const minusBtn = document.getElementById('minus-btn');
+minusBtn.addEventListener('click', function(){
+    // console.log('runing');
+    const firstElement = document.getElementById('count');
+    sum -= 1;
+    firstElement.innerText = sum;
+})
