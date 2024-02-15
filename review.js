2cf304a33a97e0bb1125d937c507fa800f34750f
Idba1 <moniraislamself@gmail.com>
2024-02-15 08:16:05 +0600

    review

--- review.js
+++ review.js
@@ -1,1 +1,19 @@
-console.log('hello');
+let serialCount = 1;
+
+document.getElementById('submit-btn').addEventListener('click', function(){
+    console.log('hello');
+
+    // const inputArea = document.getElementById('text-area');
+    // const inputValue = inputArea.value;
+
+    const inputValue = document.querySelector('textarea').value;
+    console.log(inputValue);
+
+
+    const container = document.getElementById('review');
+
+    const p = document.createElement('p');
+    p.innerText = serialCount + "." + inputValue;
+    serialCount += 1;
+    container.appendChild(p);
+})
